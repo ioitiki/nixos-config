2c318ab87df79b0e380d2a7a094eca9dd6993130
Andy Malkin <andy@chainstarters.com>
2023-11-17 09:48:37 -0800

    upgrade bun to 1.0.12

--- bun-overlay.nix
+++ bun-overlay.nix
@@ -2,10 +2,10 @@ self: super:
 
 {
   bun = super.bun.overrideAttrs (oldAttrs: rec {
-    version = "1.0.11";
+    version = "1.0.12";
     src = super.fetchurl {
       url = "https://github.com/oven-sh/bun/releases/download/bun-v${version}/bun-linux-x64.zip";
-      sha256 = "a53f7e19c84d0b7be67858131741b3cf22f3581ac241c47f0c54552b60a71c2c";
+      sha256 = "f845add7f62d1b3266fe54413eb7f544ce7174c545316bab44d05000200b7120";
     };
   });
 }
